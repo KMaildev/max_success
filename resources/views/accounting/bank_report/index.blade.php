6cd2d79c1b4a3d691b6d87af4f182c1584256c0c
KMail <kmailsoftwaredevelopment@gmail.com>
2023-07-08 14:52:20 +0630

    update

--- resources/views/accounting/bank_report/index.blade.php
+++ resources/views/accounting/bank_report/index.blade.php
@@ -19,9 +19,15 @@
                                         <th class="text-white w-5">Bank</th>
                                         <th class="text-white w-5">Bank In</th>
                                         <th class="text-white w-5">Bank Out</th>
+                                        <th class="text-white w-5">Total Balance	</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @php
+                                        $total_income = [];
+                                        $total_expense = [];
+                                        $total_total_balance = [];
+                                    @endphp
                                     @foreach ($chartof_accounts as $key => $chartof_account)
                                         <tr>
                                             <td style="text-align: center;">
@@ -30,20 +36,58 @@
 
                                             <td style="text-align: center;">
                                                 {{ $chartof_account->description }}
-                                                /
-                                                {{ $chartof_account->id }}
                                             </td>
 
-                                            <td>
-                                                {{ number_format($chartof_account->bank_cash_report->sum('income'), 2) }}
+                                            <td style="text-align: right;">
+                                                @php
+                                                    $income = $chartof_account->bank_cash_report->sum('income');
+                                                    $total_income[] = $income;
+                                                    echo number_format($income, 2);
+                                                @endphp
                                             </td>
 
-                                            <td>
-                                                {{ number_format($chartof_account->bank_cash_report->sum('expense'), 2) }}
+                                            <td style="text-align: right;">
+                                                @php
+                                                    $expense = $chartof_account->bank_cash_report->sum('expense');
+                                                    $total_expense[] = $expense;
+                                                    echo number_format($expense, 2);
+                                                @endphp
+                                            </td>
+
+                                            <td style="text-align: right;">
+                                                @php
+                                                    $total_balance = $income - $expense;
+                                                    echo number_format(abs($total_balance), 2);
+                                                    $total_total_balance[] = $total_balance;
+                                                @endphp
                                             </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
+
+                                <tr>
+                                    <th colspan="2">Total</th>
+                                    <th style="text-align: right; font-weight: bold">
+                                        @php
+                                            $total_income = array_sum($total_income);
+                                            echo number_format($total_income, 2);
+                                        @endphp
+                                    </th>
+                                    
+                                    <th style="text-align: right; font-weight: bold">
+                                        @php
+                                            $total_expense = array_sum($total_expense);
+                                            echo number_format($total_expense, 2);
+                                        @endphp
+                                    </th>
+
+                                    <th style="text-align: right; font-weight: bold">
+                                        @php
+                                            $total_total_balance = array_sum($total_total_balance);
+                                            echo number_format(abs($total_total_balance), 2);
+                                        @endphp
+                                    </th>
+                                </tr>
                             </table>
                         </div>
                     </div>
